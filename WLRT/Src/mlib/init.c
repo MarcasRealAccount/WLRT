1e39436dd9ac75d73258cd4b81e98b16c2e58b6d
MarcasRealAccount <thetremine.lumicks@gmail.com>
2023-08-24 13:22:20 +0200

    Remove profiler

--- WLRT/Src/mlib/init.c
+++ WLRT/Src/mlib/init.c
@@ -2,15 +2,8 @@
 #include "exit.h"
 #include "filewatcher.h"
 #include "log.h"
-#include "profiler.h"
 #include "thread.h"
 #include "tim.h"
-
-static void mprofiler_on_exit(void* data)
-{
-	(void) data;
-	mprofiler_deinit();
-}
 
 static void mtime_on_exit(void* data)
 {
@@ -40,7 +33,6 @@ bool mlib_init()
 {
 	if (!mexit_init())
 		return false;
-	massert_register(mprofiler_init(), 1, &mprofiler_on_exit, NULL);
 	massert_register(mtime_init(), 1, &mtime_on_exit, NULL);
 	massert_register(mthread_init(), 1, &mthread_on_exit, NULL);
 	massert_register(mlog_init(), 1, &mlog_on_exit, NULL);
